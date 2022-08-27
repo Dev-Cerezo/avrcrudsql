87bdeb465759413957584950936325f0a5319b03
ING.D.G.S Abraham Vieyra Razo <AbrahamV@GRUPOELCEREZO.MX>
2022-08-27 09:18:06 -0500

    se agragaron las variables de entorno

--- src/config.js
+++ src/config.js
@@ -5,5 +5,9 @@ config();
 console.log(process.env.NICKNAME)
 
 export default {
-    port:4000
+    port:process.env.DB_USER || 300,
+    dbUser: process.env.DB_USER || '',
+    dbPassword: process.env.DB_PASSWORD || '',
+    dbServer: process.env.DB_SERVER || '',
+    dbDatabase: process.env.DB_DATABASE || ''
 }
--- src/database/conection.js
+++ src/database/conection.js
@@ -1,10 +1,10 @@
 import sql from 'mssql' //se importa el modulo
-
+import config from '../config';
 const dbsetting = { //se configuran los parametros de la base de datos a la que se va a hacer conexion
-user: 'user',
-password: 'Cerezo.2022',
-server: '192.168.1.200',
-database: 'webstoreavr',
+  user: config.dbUser,
+  password: config.dbPassword,
+  server: config.dbServer,
+  database: config.dbDatabase,
    options: {
      encrypt: false, // for azure
      trustServerCertificate: false // change to true for local dev / self-signed certs
@@ -21,3 +21,4 @@ database: 'webstoreavr',
   }
 
   export {sql}
+  
